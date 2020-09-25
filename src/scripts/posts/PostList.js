616bd8e84f64124143ae524e0311b9c3c4dc9bc6
ste163 <samtedwards@pm.me>
2020-09-25 10:59:46 -0400

    added more comments and requirements for function

--- src/scripts/posts/PostList.js
+++ src/scripts/posts/PostList.js
@@ -2,3 +2,9 @@
     // import all needed scripts for the posts section
     // render all elements
     // handle  eventHub clicks
+
+// Requirements:
+    // create posts
+    // Posts must be prepended with user's name (and a timestamp)
+    // sort posts so the most recent is at bottom
+    // Delete posts
