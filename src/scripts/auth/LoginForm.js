2fe12bcc64d37474fc6cbdcdb45ddbfc3597970a
Terra Roush <terra.roush@gmail.com>
2020-09-25 14:40:14 -0500

    called functions for rendering login and register forms; added condition if unregistered user tries to login; added zipcode to collect info for weather

--- src/scripts/auth/LoginForm.js
+++ src/scripts/auth/LoginForm.js
@@ -1,36 +1,44 @@
+// this is where the HTML form will populate
 const contentTarget = document.querySelector(".auth--login")
 const eventHub = document.querySelector(".container")
 
-eventHub.addEventListener("userAuthenticated", e => {
-    contentTarget.innerHTML = ""
-})
-
+// when a click is registered on the login button...
 eventHub.addEventListener("click", e => {
     if (e.target.id === "login__button") {
+        // store value of the user input in username field to a likely named variable
         const username = document.querySelector("#login__username").value
 
+        // return that user found by username
         return fetch(`http://localhost:8088/users?username=${username}`)
             .then(response => response.json())
             .then(users => {
+                // an array returns. if there is something in it, take the first index(0) and store the following key/value data in session storage
                 if (users.length > 0) {
                     const user = users[0]
                     sessionStorage.setItem("activeUser", user.id)
+                    sessionStorage.setItem("userName", user.username)
+                    // tell the app that the user has been made legit
                     eventHub.dispatchEvent(new CustomEvent("userAuthenticated"))
+                // if not a legit user, rerender a blank form and alert user to register
+                } else {
+                   LoginForm();
+                    window.alert("Not a legit user. Register first!  😭")
                 }
             })
     }
 })
 
-
+// function that contains login form HTML
 const render = () => {
-    contentTarget.innerHTML += `
+    contentTarget.innerHTML = `
         <section class="login">
-            <input id="login__username" type="text" placeholder="Enter your username">
+            <input id="login__username" type="text" placeholder="Username">
             <button id="login__button">Log In</button>
         </section>
     `
 }
 
+// this function tells the app to render the login form
 export const LoginForm = () => {
     render()
 }
--- src/scripts/auth/RegisterForm.js
+++ src/scripts/auth/RegisterForm.js
@@ -1,36 +1,49 @@
+// this is where the HTML form will populate
 const contentTarget = document.querySelector(".auth--register")
 const eventHub = document.querySelector(".container")
 
+//when a user is deemed legit, a classlist is added to the entire authorization container to make it disappear.
 eventHub.addEventListener("userAuthenticated", e => {
-    contentTarget.innerHTML = ""
+    document.getElementById("authContainer").classList.add("toggleAuth")
 })
 
 
 eventHub.addEventListener("click", e => {
+    // when the click is on the register button...
     if (e.target.id === "register--button") {
+        // store values of username,email and zipcode and assign them to variables
         const username = document.querySelector("#register--username").value
         const email = document.querySelector("#register--email").value
+        const zipcode = document.querySelector("#register--zipcode").value
 
+        //if these fields are not empty
         if (username !== "" && email !== "") {
-            // Does the user exist?
+            //Is this username in our database of users?
             fetch(`http://localhost:8088/users?username=${username}`)
             .then(response => response.json())
             .then(users => {
+                // if no users come back, then POST, or add that new user to our database
                 if (users.length === 0) {
                     fetch("http://localhost:8088/users", {
                         "method": "POST",
                         "headers": {
                             "Content-Type": "application/json"
                         },
+                        // make this data into json suitable strings
                         body: JSON.stringify({
                             "username": username,
-                            "email": email
+                            "email": email,
+                            "zipcode": zipcode
                         })
                     })
                         .then(response => response.json())
+                        // then save these key/value pairs of data in session storage
                         .then((newUser) => {
                             sessionStorage.setItem("activeUser", newUser.id)
-
+                            sessionStorage.setItem("userName", newUser.username)
+                            sessionStorage.setItem("email", newUser.email)
+                            sessionStorage.setItem("zipcode", newUser.zipcode)
+                        // tell the app that the user has been made legit
                             eventHub.dispatchEvent(new CustomEvent("userAuthenticated"))
                         })
 
@@ -43,18 +56,20 @@ eventHub.addEventListener("click", e => {
     }
 })
 
-
+// function that contains registration form HTML
 const render = () => {
     contentTarget.innerHTML += `
         <section class="register">
-            <input id="register--username" type="text" placeholder="Enter your username">
-            <input id="register--email" type="text" placeholder="Enter your email address">
+            <input id="register--username" type="text" placeholder="Username">
+            <input id="register--email" type="text" placeholder="Email">
+            <input id="register--zipcode" type="text" placeholder="Zipcode">
 
             <button id="register--button">Register</button>
         </section>
     `
 }
 
+// this function tells the app to render the registration form
 export const RegisterForm = () => {
     render()
 }
