a49e245ca04e85537d32cc9c5c9305a9b4daafa7
Terra Roush <terra.roush@gmail.com>
2020-09-28 10:50:36 -0500

    fixed parameter in fetch call to match

--- src/scripts/weather/WeatherProvider.js
+++ src/scripts/weather/WeatherProvider.js
@@ -9,7 +9,7 @@ let weather = [];
 export const useWeather = () => weather.slice();
 // a function that gets current weather when given a zipcode
 export const getWeather = (zip) => {
-  return fetch(`https://api.openweathermap.org/data/2.5/weather?zip=${zipcode}&units=imperial&appid=${keys()}`)
+  return fetch(`https://api.openweathermap.org/data/2.5/weather?zip=${zip}&units=imperial&appid=${keys()}`)
   .then((response) => response.json()
   .then((parsedWeather) => {
       weather = parsedWeather.list
