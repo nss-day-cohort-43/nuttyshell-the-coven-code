fb8c53aa84be093e80a8d4641ce3855e5cf5b0c4
Terra Roush <terra.roush@gmail.com>
2020-09-27 21:23:08 -0500

    commentson code

--- src/scripts/weather/WeatherProvider.js
+++ src/scripts/weather/WeatherProvider.js
@@ -1,12 +1,13 @@
 import defaultExport from "../Settings.js"
-
+// creates ability to use api key
 const keys = () => {
     return defaultExport.weatherKey
 }
+// defines variable that can change as the data returned changes
 let weather = [];
-
+// this is a copy of the weather array
 export const useWeather = () => weather.slice();
-
+// a function that gets current weather when given a zipcode
 export const getWeather = (zip) => {
   return fetch(`https://api.openweathermap.org/data/2.5/weather?zip=${zipcode}&units=imperial&appid=${keys()}`)
   .then((response) => response.json()
