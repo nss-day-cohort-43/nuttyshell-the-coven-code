f673cb077db9f665c2be3f8867a9783d9b68bf1d
Tristanwyatt <tristan7306@gmail.com>
2020-09-29 22:05:26 -0500

    Lets a user know that user does not exist when adding a friend

--- src/scripts/friends/FriendProvider.js
+++ src/scripts/friends/FriendProvider.js
@@ -52,3 +52,17 @@ export const deleteFriend = (friendsId) => {
     })
     .then(dispatchFriendStateChangeEvent)
 }
+
+let users;
+
+export const useUsers = () => {
+    return users.slice()
+}
+
+export const getUsers = () => {
+    return fetch (`http://localhost:8088/users/`)
+    .then(response => response.json())
+    .then(parsedUsers => {
+        users = parsedUsers
+    })
+}
