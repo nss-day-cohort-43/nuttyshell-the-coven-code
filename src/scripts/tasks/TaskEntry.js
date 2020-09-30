3252bc0efe57939453336c1a5b918549055ce9a8
Hanako Hashiguchi <hanako.e.hashiguchi@gmail.com>
2020-09-29 19:33:46 -0500

    added checkbox functionality with the help of Sam

--- src/scripts/tasks/TaskEntry.js
+++ src/scripts/tasks/TaskEntry.js
@@ -1,9 +1,10 @@
+// Authored by Hanako Hashiguchi and Sam Edwards
+
 import { completeTaskEntry } from "./TaskDataProvider.js"
 
 const tasksContainer = document.querySelector(".tasks")
 
 tasksContainer.addEventListener("click", clickEvent => {
-    debugger;
     if(clickEvent.target.id.startsWith("complete--")) {
         const [prefix, id] = clickEvent.target.id.split("--")
         const numId = parseInt(id)
@@ -15,10 +16,17 @@ tasksContainer.addEventListener("click", clickEvent => {
     }
 })
 
+const checkCheckBox = (task, completedBoolean) => {
+    const notCompleted = `<input class="completed" title="Completed Task" id="complete--${task.id}" type="checkbox"/>`
+    const completed = `<input checked class="completed" title="Completed Task" id="complete--${task.id}" type="checkbox"/>`
+
+    return completedBoolean ? completed : notCompleted;
+}
+
 export const taskEntry = task => {
     return `
         <li id="task--${task.id}" class="taskEntry">
-            <input class="completed" title="Completed Task" id="complete--${task.id}" type="checkbox"/>
+            ${checkCheckBox(task, task.complete)}
             <div id="taskName--${task.id}" value="${task.name}">${task.name}</div>
             <div id="dueDate--${task.id}" value="${task.dueDate}">Due Date: ${task.dueDate}</div> 
             <button class="delete" title="Delete" id="delete--${task.id}">✘</button>
